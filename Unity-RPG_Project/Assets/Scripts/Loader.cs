354fb20d45732453f4d5f2357f61c61f52a0dd83
Emmilio Segovia <Emmilio Segovia>
2016-12-18 02:52:19 -0800

    Reorganized Project files

---
+++ Unity-RPG_Project/Assets/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager; //reference to the GameManager object
+
+	void Awake () { //called when this script component starts
+		//Note this is the static variable since a GameManager is a class, not a reference
+		if (GameManager.instance == null) //if there is no GameManager currently,
+			Instantiate(gameManager); //instantiate it
+	}
+}
