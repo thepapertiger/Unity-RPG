51e3b3106582646bd850e971275202a3860729b7
Emmilio Segovia <Emmilio Segovia>
2016-12-18 02:28:13 -0800

    Added a Project with Player and GameManager (has Windows line-endings)

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * I created this placeholder so GameManager can compile
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		/*
+	//the Monsters can add themselves to the GameManager's list by calling:
+		GameManager.AddMonsterToList (Monster this); 
+		*/
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
