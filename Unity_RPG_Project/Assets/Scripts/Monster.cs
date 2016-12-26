3d039c41edd4250885c6540f7e4d220230e16e34
thepapertiger <skuo006@ucr.edu>
2016-12-26 15:49:50 -0800

    Added Player detection to the monster script

--- Unity_RPG_Project/Assets/Scripts/Monster.cs
+++ Unity_RPG_Project/Assets/Scripts/Monster.cs
@@ -9,9 +9,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MovingObject {
-
+   
     private Animator animator_monster;
     private GameObject player;
+    private Transform target;
 
 	// Use this for initialization
 	protected override void Start () {
@@ -19,7 +20,7 @@ public class Monster : MovingObject {
 	//the Monsters can add themselves to the GameManager's list by calling:
 		GameManager.AddMonsterToList (Monster this); 
 		*/
-        GameManager.AddMonsterToList(Monster this); //add monsters to GameManager's list
+        GameManager.Instance.AddMonsterToList(this); //add monsters to GameManager's list
         player = GameObject.FindGameObjectWithTag("Player").gameObject;
         animator_monster = GetComponent<Animator>();
         base.Start();
@@ -48,6 +49,9 @@ public class Monster : MovingObject {
     protected override void OnCantMove<T>(T component)
     {
         //TODO: define what the monster does when run into a blocking object
+
+        //Declare hitPlayer and set it to equal the encountered componenet.
+        Player hitPlayer = component as Player;
     }
 
     /// <summary>
@@ -55,11 +59,30 @@ public class Monster : MovingObject {
     /// </summary>
     private void DetectPlayer()
     {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f, 1 << LayerMask.NameToLayer("Player"));
+        int xDir = 0;
+        int yDir = 0;
+        //If the difference in positions is approximately zero (Epsilon) do the following:
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            //If the y coordinate of the target's (player) position is greater than the y coordinate of this enemy's position set y direction 1 (to move up). If not, set it to -1 (to move down).
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        //If the difference in positions is not approximately zero (Epsilon) do the following:
+        else
+        {
+            //Check if target x position is greater than enemy's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
 
-<<<<<<< HEAD
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if(colliders[i].gameObject.tag == "Player")
+            {
+                AttemptMove<Player>(xDir, yDir);
+            }
+        }
     }
-=======
-	}
+	
 
->>>>>>> ce98d42e47466566a4bb2364e09b67ad1f5545cc
 }
