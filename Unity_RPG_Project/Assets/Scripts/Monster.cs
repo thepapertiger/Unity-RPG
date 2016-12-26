555ea22701f18dba5f9021d6831f48745a3cea5f
thepapertiger <skuo006@ucr.edu>
2016-12-26 14:05:01 -0800

    Added Monster.cs

--- Unity_RPG_Project/Assets/Scripts/Monster.cs
+++ Unity_RPG_Project/Assets/Scripts/Monster.cs
@@ -8,19 +8,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour {
+public class Monster : MovingObject {
+
+    private Animator animator_monster;
+    private GameObject player;
 
 	// Use this for initialization
-	void Start () {
-		/*
+	protected override void Start () {
+        /*
 	//the Monsters can add themselves to the GameManager's list by calling:
 		GameManager.AddMonsterToList (Monster this); 
 		*/
+        GameManager.AddMonsterToList(Monster this); //add monsters to GameManager's list
+        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        animator_monster = GetComponent<Animator>();
+        base.Start();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        base.AttemptMove<T>(xDir, yDir);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //start battle screen, get component of the battle game object, or maybe the game manager will handle this
+            Debug.Log("Monster wants to battle!");
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        //TODO: define what the monster does when run into a blocking object
+    }
+
+    /// <summary>
+    /// if player reaches the monster's line of sight, the monster chases after the player
+    /// </summary>
+    private void DetectPlayer()
+    {
+
+    }
 }
